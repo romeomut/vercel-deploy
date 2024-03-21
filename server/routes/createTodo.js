132f4cb781eddc71791b5857ef795e2da37e3c65
romeomut <mut_mut@ukr.net>
2024-03-21 20:15:19 +0200

    21032024 2015

--- server/routes/createTodo.js
+++ server/routes/createTodo.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/create-todo", (req, res) => {
-  const { text } = req.body;
+  // const { text } = req.body;
   return res.status(200).json({ message: "Super" });
 });
 
